686dfb66ef134ffdc593228325a7513a580fffd4
Marcin Cieslik <mcieslik@med.umich.edu>
2017-03-20 18:14:22 +0000

    instructions (9)

--- docker/build_image_codac.sh
+++ docker/build_image_codac.sh
@@ -11,7 +11,7 @@ export INCHWORM_BIN=$ROOT/deps/bin/inchworm
 cp $CODAC_FILE $HERE/context/codac
 cp $SAMTOOLS_BIN $HERE/context/codac
 cp $INCHWORM_BIN $HERE/context/codac
-docker build --build-arg CODAC_VER=$CODAC_VER -t codac:$CODAC_REL $HERE/context/codac/codac
+docker build --build-arg CODAC_VER=$CODAC_VER -t codac:$CODAC_REL $HERE/context/codac
 rm $HERE/context/codac/$(basename $CODAC_FILE) $HERE/context/codac/samtools $HERE/context/codac/inchworm
 docker tag codac:$CODAC_REL codac:latest
 docker save -o $HERE/image/codac_$CODAC_REL.tar codac:$CODAC_REL
