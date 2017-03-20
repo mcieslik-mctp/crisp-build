0c7c6134c9579defa68b301d85b6b1c3b95d742d
Marcin Cieslik <mcieslik@med.umich.edu>
2017-03-20 17:34:48 +0000

    instructions (4)

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ Step 1: Prepare build environment
 ```
 git clone https://github.com/mctp/crisp-build
 cp <<NOVOALIGN LICENSE KEY>> crisp-build/novoalign.lic
-
+cd crisp-build
 ```
 
 Step 2: Build GXCORE Docker image
 ```
+bash docker/build_image_gxcore.sh
 ```
 
 Step 3: Prepare dependencies
--- docker/build_image_gxcore.sh
+++ docker/build_image_gxcore.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
-## GXCORE
-export GXCORE_REL=1.5.0-r1
-export USER=$USER
-export USERID=`id -u`
-docker build -t gxcore:$GXCORE_REL --build-arg user=$USER --build-arg userid=$USERID context/gxcore
+HERE="$(readlink -f $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
+GXCORE_REL=1.5.0-r1
+USER=$USER
+USERID=`id -u`
+docker build -t gxcore:$GXCORE_REL --build-arg user=$USER --build-arg userid=$USERID $HERE/context/gxcore
 docker tag gxcore:$GXCORE_REL gxcore:latest
-
-mkdir -p image
-docker save -o image/gxcore_$GXCORE_REL.tar gxcore:$GXCORE_REL
+mkdir -p $HERE/image
+docker save -o $HERE/image/gxcore_$GXCORE_REL.tar gxcore:$GXCORE_REL
