354c11726d30a23fd76bbbd3bc83bdb4a87e1721
Marcin Cieslik <mcieslik@med.umich.edu>
2017-03-20 18:05:27 +0000

    instructions (7)

--- docker/build_image_crisp.sh
+++ docker/build_image_crisp.sh
@@ -8,7 +8,7 @@ CRISP_REL=$CRISP_VER-r1
 CRISP_PATH=$ROOT/deps/crisp
 CRISP_FILE=$CRISP_PATH/crisp_novo_$CRISP_VER.tar.gz
 cp $CRISP_FILE $HERE/context/crisp
-docker build --build-arg CRISP_REL=$CRISP_REL -t crisp:$CRISP_REL $HERE/context/crisp
+docker build --build-arg CRISP_VER=$CRISP_VER -t crisp:$CRISP_REL $HERE/context/crisp
 rm $HERE/context/crisp/$(basename $CRISP_FILE)
 docker tag crisp:$CRISP_REL crisp:latest
 docker save -o $HERE/image/crisp_$CRISP_REL.tar crisp:$CRISP_REL
